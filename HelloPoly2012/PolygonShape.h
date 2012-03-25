904a66a8795f8783a549785e3938eb45e81edca9
Cameron Lane <lane.cameron@gmail.com>
2012-03-25 11:55:00 -0400

    committed initial files

---
+++ HelloPoly2012/PolygonShape.h
@@ -0,0 +1,13 @@
+//
+//  PolygonShape.h
+//  HelloPoly2012
+//
+//  Created by Cameron Lane on 3/25/12.
+//  Copyright (c) 2012 Adamanteus Software, LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PolygonShape : NSObject
+
+@end
---
+++ HelloPoly2012/PolygonShape.m
@@ -0,0 +1,13 @@
+//
+//  PolygonShape.m
+//  HelloPoly2012
+//
+//  Created by Cameron Lane on 3/25/12.
+//  Copyright (c) 2012 Adamanteus Software, LLC. All rights reserved.
+//
+
+#import "PolygonShape.h"
+
+@implementation PolygonShape
+
+@end
